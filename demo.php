badaf754402244ccc8e4e21d89c4bdcf28fb1a60
Hans H Chou <hchou@fas.harvard.edu>
2013-09-29 10:00:12 -0400

    Initial add of demo.php (day-or-night presentation code), updating CSS and day-or-night.php (day-or-night logic code)

---
+++ demo.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php require_once('day-or-night.php'); ?>
+    <link href="css/grass.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body class="<?=$themeClass?>">
+    <p>Text and background colors for the "day" and "night" themes are defined in the CSS as classes.</p>
+    <p>The active theme is: <?=$themeClass?></p>
+</body>
+</html>
