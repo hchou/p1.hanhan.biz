6cfa33e8347d12b4bc8b6736e3bc69f33f8f93a0
Hans H Chou <hchou@fas.harvard.edu>
2013-09-30 19:21:38 -0400

    Minor updates to the presentation code.

--- demo.php
+++ demo.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <?php require_once('day-or-night.php'); ?>
+    <?php require_once('logic.php'); ?>
     <link href="css/grass.css" rel="stylesheet" type="text/css">
 
 </head>
@@ -9,5 +9,9 @@
 <body class="<?=$themeClass?>">
     <p>Text and background colors for the "day" and "night" themes are defined in the CSS as classes.</p>
     <p>The active theme is: <?=$themeClass?></p>
+    <div class="box">box!</div>
+    <br>
+    <?=$boxes?>
+
 </body>
 </html>
